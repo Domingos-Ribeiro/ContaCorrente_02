690f39f789076e1a1df9b0ed8f43db6740eff986
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-24 12:48:42 +0100

    admin

---
+++ Controllers/SoParaAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContaCorrente_02.Controllers
+{
+    public class SoParaAdminController : Controller
+    {
+        // GET: SoParaAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SoParaMembrosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContaCorrente_02.Controllers
+{
+    public class SoParaMembrosController : Controller
+    {
+        // GET: SoParaMembros
+        public ActionResult Index()
+        {
+            // Se nao há autenticação através da variável de sessão "AuthState",
+            // então, direciona para a página de erro.
+            string estado;
+            try
+            {
+                estado = Session["AuthState"].ToString();
+
+            }
+            catch (Exception)
+            {
+                estado = "";
+            }
+
+            // Se há autenticação e o utilizador é o admin, pode aceder.
+
+            if (estado == "Autiticated")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Erro");
+            }
+        }
+    }
+}
