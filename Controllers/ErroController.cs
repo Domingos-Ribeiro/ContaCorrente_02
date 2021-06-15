de04a3b4a89b72a496ddf3a6f1ca65b8c439faaf
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-06-15 23:46:37 +0100

    views

--- Controllers/ErroController.cs
+++ Controllers/ErroController.cs
@@ -9,6 +9,7 @@ namespace ContaCorrente_02.Controllers
     public class ErroController : Controller
     {
         // GET: Erro
+        // Ao chamar a view escolher o que deve aparecer para o uyilizador
         public ActionResult Index()
         {
             return View();
